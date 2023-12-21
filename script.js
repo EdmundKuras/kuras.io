9eac9f3009c9bab5912f567faa50879bd00974f3
goldedda <edmundk@fastmail.com>
2023-12-21 17:52:51 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Your dynamic JavaScript code can go here
     console.log("Welcome to Kuras.io!");
+	
+	document.addEventListener("DOMContentLoaded", function() {
+    fetch('reviews.json')
+    .then(response => response.json())
+    .then(data => {
+        const reviewsContainer = document.getElementById('reviews');
+        reviewsContainer.innerHTML = ''; // Clear the loading text
+
+        data.forEach(review => {
+            const div = document.createElement('div');
+            div.className = 'review';
+            div.innerHTML = `<h2>${review.product}</h2><p>${review.review}</p>`;
+            reviewsContainer.appendChild(div);
+        });
+    })
+    .catch(error => {
+        document.getElementById('reviews').innerText = 'Failed to load reviews.';
+    });
+});
+
 });
